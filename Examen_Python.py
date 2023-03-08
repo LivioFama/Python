6a0b068184fbd74a42fc2608f3f5e851b265f323
LivioFama <123375574+LivioFama@users.noreply.github.com>
2023-03-08 13:15:03 +0100

    Add files via upload

---
+++ Examen_Python.py
@@ -0,0 +1,45 @@
+import requests.api
+import re
+import yaml
+
+url = 'https://api.domainsdb.info/v1/domains/search?domain=syntra.be'
+response = requests.get(url)
+data = response.text
+output=[]
+
+
+#create_date = re.search(r'"create_date":\s*"(.*?)"', data).group(1)
+
+create_regex = r'\d{4}'
+create_year = re.search(create_regex, response.text).group(0)
+
+create_regex = r'[-.\/](0[1-9]|1[012])'
+create_month = re.search(create_regex, response.text).group(1)
+
+create_regex = r'-(\d{2})+T'
+create_day = re.search(create_regex, response.text).group(1)
+
+
+ip_regex = r'\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b'
+ip_adres = re.search(ip_regex, response.text).group(0)
+
+land = re.search(r'"country":\s*"(.*?)"', data).group(1)
+
+provider_regex = r'(?<=\.)belnet(?=\.)'                     
+provider = re.search(provider_regex, response.text).group(0)
+
+
+
+output.append({'ip': ip_adres, 'land': land, 'Dag': create_day, 'Maand': create_month, 'Jaar': create_year, 'Provider': provider})
+
+
+if response.status_code == 200:
+    data = response.json()
+    yaml_data = yaml.dump({"Examen opdracht": output})
+
+    with open('output.yaml', 'w') as file:
+        file.write(yaml_data)
+
+    print("YAML output written to output.yaml")
+else:
+    print(f"Error fetching data: {response.status_code} - {response.text}")
